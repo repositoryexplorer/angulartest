65c065f399231464671ffc684992151f2441be0a
Przemek <przemekggg@gmail.com>
2020-05-21 15:45:52 +0200

    Initial commit

--- src/app/pages/flatplan2/printpreview/printpreview.component.ts
+++ src/app/pages/flatplan2/printpreview/printpreview.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxTemplateModule, DxButtonModule, DxPopupModule, DxPopoverModule } from 'devextreme-angular';
+import notify from 'devextreme/ui/notify';
+import {Gallery2ServiceService, House} from '../../../shared/services/gallery2-service.service';
+
+
+if(!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
 
 @Component({
   selector: 'app-printpreview',
   templateUrl: './printpreview.component.html',
-  styleUrls: ['./printpreview.component.scss']
+  styleUrls: ['./printpreview.component.scss'],
+  providers: [Gallery2ServiceService]
 })
-export class PrintpreviewComponent implements OnInit {
+export class PrintpreviewComponent {
+  houses: House[];
+  currentHouse: House;
+  popupVisible = false;
+  ADD_TO_FAVORITES = "Add to Favorites";
+  REMOVE_FROM_FAVORITES = "Remove from Favorites";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(service: Gallery2ServiceService) {
+    this.houses = service.getSpreads();
+    this.currentHouse = this.houses[0];
   }
 
+  showHouse(house: House) {
+    this.currentHouse = house;
+    this.popupVisible = true;
+  }
+
+  changeFavoriteState() {
+    let favoriteState = !this.currentHouse.Favorite;
+    let message = "This item has been "
+      + (favoriteState ? "added to" : "removed from")
+      + " the Favorites list!";
+    this.currentHouse.Favorite = favoriteState;
+
+    notify({
+        message: message,
+        width: 450
+      },
+      favoriteState ? "success" : "error",
+      2000);
+  }
 }
+
