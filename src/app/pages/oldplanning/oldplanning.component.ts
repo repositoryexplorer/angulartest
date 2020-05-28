7152fe16a06a7194c7bc1a10d957ed19efd82bd9
Przemek <przemekggg@gmail.com>
2020-05-28 09:12:09 +0200

    Header -> route path

--- src/app/pages/oldplanning/oldplanning.component.ts
+++ src/app/pages/oldplanning/oldplanning.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import {HttpClient, RequestOptions} from '@angular/common/http';
+import {DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -10,17 +10,17 @@ import {HttpClient, RequestOptions} from '@angular/common/http';
 })
 
 
-export class OldplanningComponent implements OnInit {
+export class OldplanningComponent {
   url: any;
   sessionId: string = '';
-
+  postUrl: any;
   constructor(private sanitizer: DomSanitizer, private httpClient: HttpClient) {
     var options = { params: {username: 'dev',  password: 'priintdev', db: 'aio'}};
-
-    this.httpClient.post(sanitizer.bypassSecurityTrustResourceUrl('http://localhost:40080/CometServer/linksServlet')).toPromise()
+    this.postUrl = sanitizer.bypassSecurityTrustResourceUrl('http://localhost:40080/CometServer/linksServlet');
+    this.httpClient.post('http://localhost:40080/CometServer/linksServlet', '', options).toPromise()
       .then((data: any) => {
         this.sessionId = this.getSessionIdFromData(data);
-        console.log(this.sessionId);
+        console.log(this.sessionId + " ");
         this.url = sanitizer.bypassSecurityTrustResourceUrl('http://localhost:40080/PriintPlanner/?content=dynamic&configname=panels/Planning.xml&modelname=dev01&debug=true&locale=en&sessionid='+this.sessionId);
       })
       .catch((error) => {
@@ -29,5 +29,7 @@ export class OldplanningComponent implements OnInit {
   }
 
 
-
+  private getSessionIdFromData(data: any) {
+    return '';
+  }
 }
